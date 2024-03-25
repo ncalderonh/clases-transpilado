1933959ba29e390589f384563f785f0430000fdd
Nicolás Calderón Hernández <129394494+ncalderonh@users.noreply.github.com>
2024-03-24 23:48:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,11 @@
 import Cliente from './cliente.js';
 import CalculadoraImpuestos from './caltax.js';
 
-// Creamos instancias de Cliente
+// Crea instancias de Cliente
 const cliente1 = new Cliente('Juan', 50000, 10000);
 const cliente2 = new Cliente('María', 80000, 15000);
 
-// Calculamos el impuesto total para cada cliente
+// Calcula el impuesto total para cada cliente
 const impuestoCliente1 = CalculadoraImpuestos.calcularImpuestoTotal(cliente1);
 const impuestoCliente2 = CalculadoraImpuestos.calcularImpuestoTotal(cliente2);
 
