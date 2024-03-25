5f33a82f3ea0d8eaca663d73098dda4fba9ffdf0
Nicolás Calderón Hernández <129394494+ncalderonh@users.noreply.github.com>
2024-03-24 23:42:48 -0300

    Update Main-transpilado.js

--- Main-transpilado.js
+++ Main-transpilado.js
@@ -5,11 +5,11 @@ var _caltax = _interopRequireDefault(require("./caltax.js"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 // Main.js
 
-// Creamos instancias de Cliente
+// Crea instancias de Cliente
 var cliente1 = new _cliente["default"]('Juan', 50000, 10000);
 var cliente2 = new _cliente["default"]('María', 80000, 15000);
 
-// Calculamos el impuesto total para cada cliente
+// Calcula el impuesto total para cada cliente
 var impuestoCliente1 = _caltax["default"].calcularImpuestoTotal(cliente1);
 var impuestoCliente2 = _caltax["default"].calcularImpuestoTotal(cliente2);
 console.log("El impuesto total para ".concat(cliente1.getNombre(), " es: ").concat(impuestoCliente1));
